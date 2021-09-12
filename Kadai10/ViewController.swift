af778eb22f3a14fc240185d78342f449526b4ba2
daikiumehara <daikiumehara3@gmail.com>
2021-09-13 01:37:48 +0900

    first commit

---
+++ Kadai10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kadai10
+//
+//  Created by daiki umehara on 2021/09/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
